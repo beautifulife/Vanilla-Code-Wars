0272ddc870d86f12782f03ef7eeb7814a476c5ae
Beautifulife <davidkim1018@gmail.com>
2019-02-11 00:27:01 +0900

    Make root page
    1. set express engine
    2. set express static
    3. make root url router
    
    [Ticket: X]

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-var express = require('express');
+const express = require('express');
+const path = require('path');
+const index = require('./routes/index');
 
-var index = require('./routes/index');
+const app = express();
 
-var app = express();
+// app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
+  res.send('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,79 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: '바닐라코딩' });
+  fs.readFile('data/problems.json', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const problems = JSON.parse(data);
+    const level = req.query.level || 'all';
+
+    res.render('index', {
+      title: '바닐라코딩',
+      problems,
+      level,
+    });
+  });
+});
+
+router.get('/problems/:problem_id', (req, res, next) => {
+  fs.readFile('data/problems.json', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const problems = JSON.parse(data);
+    const problem = problems.find((item) => {
+      return item.id === parseInt(req.params.problem_id);
+    });
+
+    if (!problem) {
+      res.status(404);
+    }
+
+    res.render('problem', {
+      title: '바닐라코딩',
+      problem
+    });
+  });
+});
+
+router.post('/problems/:problem_id', (req, res, next) => {
+  fs.readFile('data/problems.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    
+    const problems = JSON.parse(data);
+    const problem = problems.find((item) => {
+      return item.id === parseInt(req.params.problem.id);
+    });
+    const userSolution = req.body.solution;
+    const testResult = {
+      numberOfTests: problem.tests.length,
+      numberOfCorrectAnswer: 0,
+      results: []
+    };
+
+    problem.tests.forEach((test, index) => {
+      if (userSolution(test.code) === test.solution) {
+        testResult.numberOfCorrectAnswer++;
+        testResult.results.push([`test${index}`, true]);
+      } else {
+        testResult.results.push([`test${index}`, false]);
+      }
+    });
+  
+    if (testResult.numberOfTests === testResult.numberOfCorrectAnswer) {
+      res.render('success', {
+        title: ''
+      });
+    } else {
+      res.render('failure', {
+        title: ''
+      });
+    }
+    //함수를 받는다(텍스트로) -> parse -> 함수를 실행한다. -> 결과일치확인
+  });
 });
 
 module.exports = router;
